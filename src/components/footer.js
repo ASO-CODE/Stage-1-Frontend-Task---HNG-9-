6d73296759955055db57e5a9701138d9c20c3734
ASO-CODE <60587094+ASO-CODE@users.noreply.github.com>
2022-10-27 16:29:21 +0100

    code clean up

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,20 +5,20 @@ import ZuriLogo from "../images/Zuri.Internship_Logo.svg";
 import I4G from "../images/I4G.svg";
 
 const Footer = () => {
-    return (
-      <section id="footer">
-        <div className="socials">
-          <img src={SlackImg} alt="slack img" />
-          <img src={GithubImg} alt="github img" />
-        </div>
-        <div className="footer-line"/>
-        <div className="logos">
-          <img src={ZuriLogo} alt="zuri logo" />
-                <p>HNG Internship 9 Frontend Task</p>
-                <img src={I4G} alt="I4G logo"/>
-        </div>
-      </section>
-    );
+  return (
+    <section id="footer">
+      <div className="socials">
+        <img src={SlackImg} alt="slack img" />
+        <img src={GithubImg} alt="github img" />
+      </div>
+      <div className="footer-line" />
+      <div className="logos">
+        <img src={ZuriLogo} alt="zuri logo" />
+        <p>HNG Internship 9 Frontend Task</p>
+        <img src={I4G} alt="I4G logo" />
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
